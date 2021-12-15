00762c8aaed69d1574923509c65a4f2562795b85
Peter Blanchfield <zygomy@gmail.com>
2021-12-15 16:48:27 +0000

    Added a test html page and an action that connects to the database but does not enter the data It should sanatise the username and hash the password and print the sql insert line to the document

---
+++ adduser.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Demo Sanitse and save</title>
+    <?php
+        $username = $_POST['username'];
+        $newstr = filter_var($username, FILTER_SANITIZE_STRING);
+        $password = $_POST['password'];
+        $hashedpw = sha1($password);
+        $db = "newuserdata";
+        $server = "localhost";
+        $user = "root";
+        $pword= "";
+        $conn = new mysqli($server,$user,$pword,$db);
+        if($conn->connect_errno){
+            echo "Connection Failed";
+            exit();
+        }
+        $sql = "insert into users (username,password) values ('" .$newstr. "','" .$hashedpw. "');";
+        echo $sql;
+        $conn->close();
+    ?>
+</head>
+<body>
+    <form method="post" action="index.html">
+        
+        <input type="submit" value="Return" />
+    </form>
+</body>
+</html>
