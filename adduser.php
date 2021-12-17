3cd4fd85fdd37ee40ddada06cc937250a9fabc49
Peter Blanchfield <zygomy@gmail.com>
2021-12-17 16:55:09 +0000

    Been running some tests

--- adduser.php
+++ adduser.php
@@ -6,21 +6,33 @@
     <title>Demo Sanitse and save</title>
     <?php
         $username = $_POST['username'];
-        $newstr = filter_var($username, FILTER_SANITIZE_STRING);
         $password = $_POST['password'];
         $hashedpw = sha1($password);
         $db = "newuserdata";
         $server = "localhost";
         $user = "root";
         $pword= "";
-        $conn = new mysqli($server,$user,$pword,$db);
-        if($conn->connect_errno){
-            echo "Connection Failed";
+        $conn = mysqli_connect($server,$user,$pword,$db);
+        if(mysqli_connect_errno()){
+            echo "Connection Failed: " . mysqli_connect_error;
             exit();
         }
-        $sql = "insert into users (username,password) values ('" .$newstr. "','" .$hashedpw. "');";
-        echo $sql;
-        $conn->close();
+        //$newstr = filter_var($username, FILTER_SANITIZE_STRING);
+        $newstr = mysqli_real_escape_string($conn,$_POST['username']);
+       
+        //$sql = "insert into users (username,password) values ('" .$newstr. "','" .$hashedpw. "');";
+        $sql = "select * from users;";
+        echo $sql. "<br/>";
+        if (($result = mysqli_query($conn,$sql))) {
+            while($row = mysqli_fetch_assoc($result))
+            {
+                echo "Row is: " . $row['userName'] ."<br/>";
+            }
+        }
+        else{
+            echo "Here is " .mysqli_affected_rows($conn). "<br/>" ;
+        }
+        mysqli_close($conn);
     ?>
 </head>
 <body>
